851a845ac9858111a33c8d20886d45fd407145de
Philippa Choong <pippachoong@gmail.com>
2022-11-14 22:45:59 +1100

    project index and page initialised

---
+++ src/components/Page/Page.jsx
@@ -0,0 +1,12 @@
+import React, {Fragment} from 'react';
+import Header from '../Header';
+
+const Page = () => {
+    return (
+        <Fragment>
+            <Header/>
+        </Fragment>
+    )
+}
+
+export default Page; 
